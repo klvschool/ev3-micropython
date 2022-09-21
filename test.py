ac26d7ee84c1e41ce4c74f2d178453f11b6f2221
ford19982541 <ford19982541@gmail.com>
2022-09-21 16:51:22 +0700

    update PID

--- test.py
+++ test.py
@@ -61,7 +61,8 @@ def a_angle():
         print('Gyro Rate: {} '.format(gyro_maximum_rate -
               gyro_minimum_rate, gyro_sensor.angle()))
         if gyro_maximum_rate - gyro_minimum_rate < 2:
-            yield gyro_sensor.angle()
+            angle = gyro_sensor.angle()
+            yield angle
             break
     gyro_offset = gyro_sum / GYRO_CALIBRATION_LOOP_COUNT
 
@@ -75,7 +76,8 @@ def a_angle():
         # calculation above depends on having a certain amount of time in each
         # loop.
         if fall_timer.time() > 1000:
-            yield gyro_sensor.angle()
+            angle = gyro_sensor.angle()
+            yield angle
             break
 
         wait(TARGET_LOOP_PERIOD - single_loop_timer.time())
@@ -85,47 +87,16 @@ def a_angle():
 
 
 def robot_turn_PID(angle=0):
-    # correction = (0 - gyro.angle())*3
-    # gyro_sensor.reset_angle(0)
-    # robot.turn(angle)
-    PROPORTIONAL_GAIN = 2.7
+    PROPORTIONAL_GAIN = 1.3
     INTEGRAL_GAIN = 0.0008
     DERIVATIVE_GAIN = 0.001
     integral = 0
     derivative = 0
-    last_error_angle = angle - gyro_sensor.angle()
-    # wait(3000)
-
-    while last_error_angle != 0:
-        error_angle = angle - gyro_sensor.angle()
-        # wait(3000)
-        print('error_angle', error_angle)
-        integral = integral + error_angle
-        derivative = last_error_angle-error_angle
-
-        turn_rate = PROPORTIONAL_GAIN * error_angle + \
-            INTEGRAL_GAIN * integral + DERIVATIVE_GAIN * derivative
-        robot.drive(0, turn_rate)
-        wait(300)
-        error_angle = angle - gyro_sensor.angle()
-        # wait(3000)
-        last_error_angle = error_angle
-        # wait(TARGET_LOOP_PERIOD - single_loop_timer.time())
-    gyro_sensor.reset_angle(0)
-
-
-gyro_sensor.reset_angle(0)
-
-while True:
-    # Reset the sensors and variables.
-    left_motor.reset_angle(0)
-    right_motor.reset_angle(0)
+    
+    # left_motor.reset_angle(0)
+    # right_motor.reset_angle(0)
     fall_timer.reset()
-
-    # Calibrate the gyro offset. This makes sure that the robot is perfectly
-    # still by making sure that the measured rate does not fluctuate more than
-    # 2 deg/s. Gyro drift can cause the rate to be non-zero even when the robot
-    # is not moving, so we save that value for use later.
+    
     while True:
         gyro_minimum_rate, gyro_maximum_rate = 440, -440
         gyro_sum = 0
@@ -154,29 +125,81 @@ while True:
         # loop.
         if fall_timer.time() > 1000:
             break
-
         wait(TARGET_LOOP_PERIOD - single_loop_timer.time())
-    print(' Gyro Offset: {} \n Gyro Angle: {} \nrobot_body_rate :{}'.format(
-        gyro_offset, gyro_sensor.angle(), robot_body_rate))
+        
+    print(' Gyro Offset: {} \n Gyro Angle: {} '.format( gyro_offset, gyro_sensor.angle()))
+    
+    last_error_angle = angle -  gyro_sensor.angle()
+    while last_error_angle != 0 :
+        error_angle = angle -  gyro_sensor.angle()
+        # wait(3000)
+        print('error_angle', error_angle)
+        integral = integral + error_angle
+        derivative = last_error_angle
+
+        turn_rate = PROPORTIONAL_GAIN * last_error_angle 
+        robot.drive(0, turn_rate)
+        wait(300)
+        error_angle = angle -  gyro_sensor.angle()
+        # wait(3000)
+        last_error_angle = error_angle
+        wait(TARGET_LOOP_PERIOD - single_loop_timer.time())
+    gyro_sensor.reset_angle(0)
+
+
+gyro_sensor.reset_angle(0)
+
+# while True:
+    # Reset the sensors and variables.
+    
+
     # print(a_angle())
-    robot_turn_PID(90)
-    wait(500)
-    robot.straight(300)
-    robot_turn_PID(90)
-    wait(500)
-    robot.straight(300)
-    robot_turn_PID(90)
-    wait(500)
-    robot.straight(300)
-    robot_turn_PID(180)
-    wait(500)
-    robot.straight(300)
-    robot_turn_PID(-180)
-    wait(500)
-    robot.straight(300)
-    robot_turn_PID(-360)
-    wait(500)
-    robot.straight(300)
-    robot_turn_PID(360)
-    wait(500)
+    # # Calibrate the gyro offset. This makes sure that the robot is perfectly
+    # # still by making sure that the measured rate does not fluctuate more than
+    # # 2 deg/s. Gyro drift can cause the rate to be non-zero even when the robot
+    # # is not moving, so we save that value for use later.
+    # while True:
+    #     gyro_minimum_rate, gyro_maximum_rate = 440, -440
+    #     gyro_sum = 0
+    #     for _ in range(GYRO_CALIBRATION_LOOP_COUNT):
+    #         gyro_sensor_value = gyro_sensor.angle()
+    #         gyro_sum += gyro_sensor_value
+    #         if gyro_sensor_value > gyro_maximum_rate:
+    #             gyro_maximum_rate = gyro_sensor_value
+    #         if gyro_sensor_value < gyro_minimum_rate:
+    #             gyro_minimum_rate = gyro_sensor_value
+    #         wait(5)
+    #     print('Gyro Rate: {} '.format(gyro_maximum_rate -
+    #           gyro_minimum_rate, gyro_sensor.angle()))
+    #     if gyro_maximum_rate - gyro_minimum_rate < 2:
+    #         break
+    # gyro_offset = gyro_sum / GYRO_CALIBRATION_LOOP_COUNT
+
+    # while True:
+    #     # calculate robot body angle and speed
+    #     gyro_sensor_value = gyro_sensor.angle()
+    #     gyro_offset *= (1 - GYRO_OFFSET_FACTOR)
+    #     gyro_offset += GYRO_OFFSET_FACTOR * gyro_sensor_value
+    #     robot_body_rate = gyro_sensor_value - gyro_offset
+    #     # Make sure loop time is at least TARGET_LOOP_PERIOD. The output power
+    #     # calculation above depends on having a certain amount of time in each
+    #     # loop.
+    #     if fall_timer.time() > 1000:
+    #         break
+
+    #     wait(TARGET_LOOP_PERIOD - single_loop_timer.time())
+    # print(' Gyro Offset: {} \n Gyro Angle: {} \nrobot_body_rate :{}'.format(
+    #     gyro_offset, gyro_sensor.angle(), robot_body_rate))
+    # print(a_angle())
+robot_turn_PID(90)
+wait(500)
+robot.straight(300)
+wait(500)
+robot_turn_PID(90)
+wait(500)
+robot.straight(300)
+wait(500)
+robot_turn_PID(90)
+wait(500)
+
     # wait(3000)
